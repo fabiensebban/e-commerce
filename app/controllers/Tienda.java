c6864cad275e0841a85f556d183df00b06068a36
Fabian Sebban <fa.sebban@gmail.com>
2014-03-27 18:41:23 +0100

    main fijado + empezando la creación de productos
    Signed-off-by: Fabian Sebban <fa.sebban@gmail.com>

---
+++ app/controllers/Tienda.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import  play.db.jpa.Blob;
+
+import java.util.*;
+
+import models.*;
+
+
+public class Tienda extends Controller {
+
+    public static void catalogo() {
+        render();
+    }
+
+    public static void anadirProductoLogeo(){
+        render();
+    }
+
+}
